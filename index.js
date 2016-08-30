d2937b3828eff41279aa663b0c3b052cbdcac60d
Andrew Klein <andrew.klein.j@gmail.com>
2016-08-30 18:08:20 -0400

    pass 5 of 11 tests

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ function total() {
 
   return t
 }
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random()*100)
+  getCart().push({[item]:price})
+  console.log(`${item} has been added to your cart.`)
+
+  return cart
+}
+
+function viewCart() {
+  var str = "In your cart, you have"
+  for (var i = 0; i < cart.length; i++) {
+    var key = Object.keys(cart[i])
+    str += ` ${key[0]} at $${cart[i][key]},`
+  }
+  console.log(str.slice(0,-1) + ".")
+}
